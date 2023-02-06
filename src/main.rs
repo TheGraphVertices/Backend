6d19454bf7b85aa560956d15bf986a9db58f91dc
Jeremy Petch <jeremy.petch@outlook.com>
2023-02-06 17:37:04 +0000

    Don't panic on missing env vars, just print error and exit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #[macro_use]
 extern crate log;
 
-use std::env;
-use actix_web::{get, App, HttpServer, Responder, middleware::Logger};
+use actix_web::{get, middleware::Logger, App, HttpServer, Responder};
 use dotenvy::dotenv;
-
+use std::env;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -16,17 +15,27 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
-    let host = env::var("HOST").expect("Environment variable HOST not set.");
-    let port = env::var("PORT").expect("Environment variable PORT not set.");
+    let host = match env::var("HOST") {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Finding HOST env var failed: {e}");
+            std::process::exit(1)
+        }
+    };
+    let port = match env::var("PORT") {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Finding PORT env var failed: {e}");
+            std::process::exit(1)
+        }
+    };
     env_logger::init();
-    info!("Starting server on {}.{}",host,port);
-    HttpServer::new(move ||{
+    info!("Starting server on {}.{}", host, port);
+    HttpServer::new(move || {
         let logger = Logger::default();
-        App::new()
-            .wrap(logger)
-            .service(index)
-        })
-        .bind(format!("{}:{}",host,port))?
-        .run()
-        .await
+        App::new().wrap(logger).service(index)
+    })
+    .bind(format!("{}:{}", host, port))?
+    .run()
+    .await
 }
